367ea8b6c9308f94424b9b82a4aa453e6020306c
Alexandr Gergalo <derien001@gmal.com>
2018-07-24 02:16:12 +0300

    Add objects for User entity

---
+++ CommitteesManager.SQLDatabase/CMDB/Scripts/PostDeployment.Users.sql
@@ -0,0 +1,42 @@
+﻿--Copyright © 2018
+--Alexandr Gergalo aka Shtahet <derien001@gmail.com>
+--
+--This file is part of CommitteesManger.
+--
+--CommitteesManger is free software: you can redistribute it and/or modify
+--it under the terms of the GNU General Public License as published by
+--the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+--
+--CommitteesManger is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU General Public License for more details.
+--
+--You should have received a copy of the GNU General Public License
+--along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
+MERGE INTO CMDB.Users AS u
+USING (VALUES
+	('ADU07ROG',	999,	'Смірнов В.Л.',		'Smirnov Vladislav',	'Директор департаменту',	'vsmirnov@mycompany.org',		'4401',	NULL,		999),
+	('ADU12PFK',	999,	'Громова А.В.',		'Gromova Alexandra',	'Начальник управління',		'agromova@mycompany.org',		'4458',	'ADU07ROG', 999),
+	('Derien',		999,	'Гергало А.В.',		'Gergalo Alex',			'Спеціаліст',				'agergalo@mycompany.org',		'6380',	'ADU12PFK',	999),
+	('ADU25SRN',	999,	'Беспалова Я.Р.',	'Bespalova Yana',		'Головний економіст',		'yabespalova@mycompany.org',	'4418', 'ADU07ROG',	2),
+	('ADU75TGW',	999,	'Воронцов Д.Д.',	'Vorontsov Danylo',		'Економіст',				'dvorotsov@mycompany.org',		'4436', 'ADU25SRN',	3),
+	('ADU44FAJ',	999,	'Белова Н.Т.',		'Belova Nina',			'Молодший економіст',		'nbelova@mycompany.org',		'4467', 'ADU25SRN',	1)
+)
+AS Source (UserID, Access_typeID, User_name_UA, User_name_US, Job_title, Email, Phone_number, HeadID, User_order)
+ON u.UserID = Source.UserID
+WHEN MATCHED THEN
+	UPDATE SET	Access_typeID = Access_typeID,
+				User_name_UA = User_name_UA,
+				User_name_US = User_name_US,
+				Job_title = Job_title,
+				Email = Email,
+				Phone_number = Phone_number,
+				HeadID = HeadID, 
+				User_order = User_order
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (UserID, Access_typeID, User_name_UA, User_name_US, Job_title, Email, Phone_number, HeadID, User_order)
+VALUES (UserID, Access_typeID, User_name_UA, User_name_US, Job_title, Email, Phone_number, HeadID, User_order)
+;
---
+++ CommitteesManager.SQLDatabase/CMDB/Tables/Users.sql
@@ -0,0 +1,40 @@
+﻿--Copyright © 2018
+--Alexandr Gergalo aka Shtahet <derien001@gmail.com>
+--
+--This file is part of CommitteesManger.
+--
+--CommitteesManger is free software: you can redistribute it and/or modify
+--it under the terms of the GNU General Public License as published by
+--the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+--
+--CommitteesManger is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU General Public License for more details.
+--
+--You should have received a copy of the GNU General Public License
+--along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
+CREATE TABLE [CMDB].[Users]
+(
+	[UserID]				varchar(20) NOT NULL,
+	[Access_typeID]			int NULL,
+	[User_name_UA]			varchar(100) NOT NULL,
+	[User_name_US]			varchar(100) NOT NULL,
+	[Job_title]				varchar(255) NULL,
+	[Email]					varchar(100) NOT NULL,
+	[Phone_number]			varchar(20) NULL,
+	[HeadID]				varchar(20) NULL,
+	[User_order]			int NULL,
+	[Is_available]			bit NOT NULL DEFAULT(1),
+	CONSTRAINT PK_Users_UserID PRIMARY KEY CLUSTERED (UserID),
+	CONSTRAINT FK_AccessTypeID_AccessType_TypeID FOREIGN KEY (Access_typeID)
+		REFERENCES CMDB.AccessTypes (Type_level)
+		ON DELETE NO ACTION
+		ON UPDATE CASCADE,
+	CONSTRAINT FK_Users_PK_Users_HeadID FOREIGN KEY (HeadID)
+		REFERENCES CMDB.Users (UserID)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION	
+)
