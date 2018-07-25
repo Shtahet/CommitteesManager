232a9acf5229d7c3fe240b91eca1500e42563b77
Shtahet <derien001@gmal.com>
2018-07-25 03:13:11 +0300

    Add common post deployment script for all sript files

---
+++ CommitteesManager.SQLDatabase/CMDB/Scripts/PostDeployment.RUN_ALL.sql
@@ -0,0 +1,39 @@
+﻿--Copyright © 2018
+--Alexandr Gergalo aka Shtahet <derien001@gmail.com>
+--
+--This file is part of CommitteesManger.
+--
+--CommitteesManger is free software: you can redistribute it and/or modify
+--it under the terms of the GNU General Public License as published by
+--the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+--
+--CommitteesManger is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU General Public License for more details.
+--
+--You should have received a copy of the GNU General Public License
+--along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
+/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+:r .\PostDeployment.AccessTypes.sql
+:r .\PostDeployment.Committees.sql
+:r .\PostDeployment.Counterparties.sql
+:r .\PostDeployment.DealTypes.sql
+:r .\PostDeployment.Environment.sql
+:r .\PostDeployment.Modes.sql
+:r .\PostDeployment.Reasons.sql
+:r .\PostDeployment.Regions.sql
+:r .\PostDeployment.Statuses.sql
+:r .\PostDeployment.Users.sql
