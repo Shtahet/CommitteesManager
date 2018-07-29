c86a4fdbff3535a6399eeaeeee3c5608d66b9188
Shtahet <derien001@gmail.com>
2018-07-30 00:22:29 +0300

    Change cuple button to toggle button for main menu

--- CommitteesManager.AppUIClient/ViewModel/MainWindowViewModel.cs
+++ CommitteesManager.AppUIClient/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using CommitteesManager.AppUIClient.Infrastructure;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace CommitteesManager.AppUIClient.ViewModel
 {
-    class MainWindowViewModel
+    class MainWindowViewModel: INotifyPropertyChanged
     {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
