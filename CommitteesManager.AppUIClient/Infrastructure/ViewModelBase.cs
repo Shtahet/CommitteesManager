ed061bd20363fe1f65782c403b06009f7925ec77
Shtahet <derien001@gmail.com>
2018-08-06 00:42:15 +0300

    Realize system of create/delte View

--- CommitteesManager.AppUIClient/Infrastructure/ViewModelBase.cs
+++ CommitteesManager.AppUIClient/Infrastructure/ViewModelBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommitteesManager.AppUIClient.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -17,12 +18,41 @@ namespace CommitteesManager.AppUIClient.Infrastructure
 
     class ViewModelBase: INotifyPropertyChanged
     {
-
+        public string Name { get; set; }
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string propertyName)
         {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public static ViewModelBase GetViewModel(ViewModels viewType)
+        {
+            ViewModelBase retView = null;
+
+            switch (viewType)
+            {
+                case ViewModels.PlaneMeeting:
+                    retView = new PlaneMeetingViewModel();
+                    retView.Name = "Планування засідання";
+                    break;
+                case ViewModels.Issues:
+                    retView = new IssuesViewModel();
+                    retView.Name = "Огляд заявок";
+                    break;
+                case ViewModels.ReqistryOfDecisions:
+                    retView = new RegistryViewModel();
+                    retView.Name = "Реєстр рішень";
+                    break;
+                case ViewModels.Protocols:
+                    retView = new ProtocolsViewModel();
+                    retView.Name = "Перелік протоколів";
+                    break;
+                default:
+                    break;
+            }
+
+            return retView;
         }
     }
 }
