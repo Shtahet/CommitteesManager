893483bee076fe969823138cac4a2d92a61759cc
Shtahet <derien001@gmail.com>
2018-08-08 00:36:17 +0300

    Clear ViewModel Base from unnecessary functionality

--- CommitteesManager.AppUIClient/Infrastructure/ViewModelBase.cs
+++ CommitteesManager.AppUIClient/Infrastructure/ViewModelBase.cs
@@ -15,48 +15,9 @@ namespace CommitteesManager.AppUIClient.Infrastructure
         ReqistryOfDecisions,
         Protocols
     }
-
-
-
+    
     class ViewModelBase: INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public bool IsExpanded { get; set; } = true;
-
-        public event EventHandler<ViewModelEventArgs> CreateView;
-        public event EventHandler<ViewModelEventArgs> WantToClose;
-
-        private RelayCommand _expandCmd;
-        public RelayCommand Expand
-        {
-            get
-            {
-                if (_expandCmd == null)
-                    _expandCmd = new RelayCommand(obj => 
-                    {
-                        IsExpanded = false;
-                        OnPropertyChanged("IsExpanded");
-                    });
-
-                return _expandCmd;
-            }
-        }
-
-        private RelayCommand _closeCmd;
-        public RelayCommand Close
-        {
-            get
-            {
-                if (_closeCmd == null)
-                    _closeCmd = new RelayCommand(obj => 
-                    {
-                        WantToClose?.Invoke(this, new ViewModelEventArgs(JoinDirectionEnum.After));
-                    });
-
-                return _closeCmd;
-            }
-        }
-
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string propertyName)
         {
