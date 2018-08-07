651fde43f64a9ec52323f6f0f9fef174c4d33107
Shtahet <derien001@gmail.com>
2018-08-07 17:18:26 +0300

    Add event arg ext class for create/delete events

---
+++ CommitteesManager.AppUIClient/Infrastructure/ViewModelEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommitteesManager.AppUIClient.Infrastructure
+{
+    public enum JoinDirectionEnum
+    {
+        After,
+        Before
+    }
+    public class ViewModelEventArgs: EventArgs
+    {
+        public ViewModelEventArgs(JoinDirectionEnum joinDirection)
+        {
+            JoinType = joinDirection;
+        }
+        public JoinDirectionEnum JoinType { get; }
+    }
+}
