157bb4eec4af37086c7369c663d026d6ad0bea89
Shtahet <derien001@gmail.com>
2018-08-08 13:35:58 +0300

    Create ViewModel class for Filters

---
+++ CommitteesManager.AppUIClient/ViewModel/FilterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommitteesManager.AppUIClient.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommitteesManager.AppUIClient.ViewModel
+{
+    class FilterViewModel:ViewModelSection
+    {
+        public FilterViewModel()
+        {
+            _closeCmd = new RelayCommand(obj => { }, obj => false);
+        }
+    }
+}
