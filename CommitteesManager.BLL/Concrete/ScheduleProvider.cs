ad442617187b9298c3480cef1c2f1ee041160e89
Shtahet <derien001@gmail.com>
2018-09-13 02:31:45 +0300

    Complete function for load data

--- CommitteesManager.BLL/Concrete/ScheduleProvider.cs
+++ CommitteesManager.BLL/Concrete/ScheduleProvider.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using CommitteesManager.BLL.Abstract;
 using CommitteesManager.DAL.Abstract;
+using CommitteesManager.DAL.Model;
+using Environment = CommitteesManager.DAL.Model.Environment;
 
 namespace CommitteesManager.BLL.Concrete
 {
@@ -15,7 +17,9 @@ namespace CommitteesManager.BLL.Concrete
         public ScheduleProvider(IDataProvider provider)
         {
             _dataProvider = provider;
+            LoadData();
         }
+
         public DateTime AdmissionStartDate { get; set; }
         public DateTime AdmissionStopDate { get; set; }
         public DateTime CommitteeDate { get; set; }
@@ -43,7 +47,25 @@ namespace CommitteesManager.BLL.Concrete
 
         protected bool LoadData()
         {
-            throw new NotImplementedException();
+            IRepository<Environment> envRepo = _dataProvider.Environments;
+
+            bool loadResult = true;
+            loadResult &= DateTime.TryParse(envRepo.FindBy(x => x.EnvName == "AdmissionStartDate").FirstOrDefault().EnvValue, out DateTime admStartDate);
+            AdmissionStartDate = admStartDate;
+
+            loadResult &= DateTime.TryParse(envRepo.FindBy(x => x.EnvName == "AdmissionStopDate").FirstOrDefault().EnvValue, out DateTime admStopDate);
+            AdmissionStopDate = admStopDate;
+
+            loadResult &= DateTime.TryParse(envRepo.FindBy(x => x.EnvName == "ScheduledCommitteeDate").FirstOrDefault().EnvValue, out DateTime schedDate);
+            CommitteeDate = schedDate.Date;
+
+            loadResult &= DateTime.TryParse(envRepo.FindBy(x => x.EnvName == "LastCommitteeDate").FirstOrDefault().EnvValue, out DateTime lastComDate);
+            LastCommitteeDate = lastComDate.Date;
+
+            loadResult &= Enum.TryParse(envRepo.FindBy(x => x.EnvName == "StatusCommitteeDate").FirstOrDefault().EnvValue, out ScheduleStatus schStatus);
+            Status = schStatus;
+
+            return loadResult;
         }
     }
 }
