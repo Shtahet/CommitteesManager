78400f309050e745561efec1f0aeeb9aad63c5e9
Shtahet <derien001@gmail.com>
2018-09-13 02:32:23 +0300

    Create classes for repository and data provider

---
+++ CommitteesManager.DAL/Concrete/DataProvider.cs
@@ -0,0 +1,32 @@
+﻿using CommitteesManager.DAL.Abstract;
+using CommitteesManager.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Environment = CommitteesManager.DAL.Model.Environment;
+
+namespace CommitteesManager.DAL.Concrete
+{
+    public class DataProvider : IDataProvider
+    {
+        private CMDB _dbContext;
+        private GeneralRepository<Environment> _environmentRepo;
+        public DataProvider()
+        {
+            _dbContext = new CMDB();
+        }
+        public IRepository<Model.Environment> Environments
+        {
+            get
+            {
+                if (_environmentRepo == null)
+                {
+                    _environmentRepo = new GeneralRepository<Environment>(_dbContext);
+                }
+                return _environmentRepo;
+            }
+        }
+    }
+}
---
+++ CommitteesManager.DAL/Concrete/GeneralRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using CommitteesManager.DAL.Abstract;
+
+namespace CommitteesManager.DAL.Concrete
+{
+    public class GeneralRepository<T> : IRepository<T> where T: class, new()
+    {
+        private DbContext _dbContext;
+        private DbSet<T> dbSet;
+        public GeneralRepository(DbContext incomingContext)
+        {
+            _dbContext = incomingContext;
+            dbSet = incomingContext.Set<T>();
+        }
+        public void AddOrUpdate(T obj)
+        {
+            dbSet.AddOrUpdate(obj);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(object key)
+        {
+            T efObj = dbSet.Find(key);
+            dbSet.Remove(efObj);
+            _dbContext.SaveChanges();
+        }
+
+        public IQueryable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            return dbSet.Where(predicate);
+        }
+
+        public T Get(object key)
+        {
+            return dbSet.Find(key);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return dbSet;
+        }
+    }
+}
