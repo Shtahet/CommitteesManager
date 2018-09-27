4dc2fb1db3641313bb3d6b9312957711ce511301
Shtahet <derien001@gmail.com>
2018-09-27 19:24:12 +0300

    Add Report service and model for grouping data

---
+++ CommitteesManager.BLL/Abstract/IReportService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommitteesManager.BLL.Model;
+
+namespace CommitteesManager.BLL.Abstract
+{
+    public interface IReportService
+    {
+        IQueryable<RegistryRecord> GetRegistryQuery();
+    }
+}
---
+++ CommitteesManager.BLL/Concrete/ReportProvider.cs
@@ -0,0 +1,41 @@
+﻿using CommitteesManager.BLL.Abstract;
+using CommitteesManager.BLL.Model;
+using CommitteesManager.DAL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommitteesManager.BLL.Concrete
+{
+    public class ReportProvider : IReportService
+    {
+        private IDataProvider _dataProvider;
+        public ReportProvider(IDataProvider incomingDataProvider)
+        {
+            _dataProvider = incomingDataProvider;
+        }
+        public IQueryable<RegistryRecord> GetRegistryQuery()
+        {
+            return _dataProvider.Deals.GetAll()
+                .Select(d => new RegistryRecord()
+                {
+                    Protocol_number = d.Agenda.Protocol.Protocol_number,
+                    Protocol_date = d.Agenda.Protocol.Protocol_date,
+                    EXPIRIOD_DATE_DECISION = d.Agenda.Protocol.EXPIRIOD_DATE_DECISION,
+                    User_name_UA = d.Agenda.Protocol.User.User_name_UA,
+                    Mode_name_UA = d.Agenda.Protocol.Mode.Mode_name_UA,
+                    Committee_name_UA = d.Agenda.Protocol.Committee.Committee_name_UA,
+                    Committe_status = d.Agenda.Protocol.Status.Status_name_UA,
+                    Region_name_UA = d.Agenda.Region.Region_name_UA,
+                    Contractor_Name = d.Agenda.Contractor_Name,
+                    Contract_number = d.Agenda.Contract_number,
+                    Deal_name_UA = d.DealType.Deal_name_UA,
+                    Deal_status = d.Status.Status_name_UA,
+                    Reason_name_UA = d.Reason.Reason_name_UA
+                });
+        }
+    }
+}
